11a8d3bafd45983604c9e598361d69f65c5b8409
Anhiu-0 <natsu7x7x777@gmail.com>
2024-10-29 13:00:18 +0700

    Changes in the adapter's flashcard

--- app/src/main/java/com/kewwi/quickmem/adapter/flashcard/SetAllAdapter.java
+++ app/src/main/java/com/kewwi/quickmem/adapter/flashcard/SetAllAdapter.java
@@ -22,12 +22,15 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
+//quản lý hiển thị các tập hợp thẻ trong recyclerview và xử lý sự kiện khi người  dùng tương tác
 
+// SetAllAdapter: Là adapter quản lý và hiển thị các tập hợp thẻ trong RecyclerView.
 public class SetAllAdapter extends RecyclerView.Adapter<SetAllAdapter.SetsViewHolder> {
 
-    private final Context context;
-    private final ArrayList<FlashCard> sets;
+    private final Context context;//sử dụng trong các thao tác chuyển đổi ngữ cảnh
+    private final ArrayList<FlashCard> sets;//danh sách các tập hợp thẻ sẽ đc hiển thị
 
+    //hàm khởi tạo adapter với context và danh sách tập hợp thẻ
     public SetAllAdapter(Context context, ArrayList<FlashCard> sets) {
         this.context = context;
         this.sets = sets;
@@ -36,47 +39,58 @@ public class SetAllAdapter extends RecyclerView.Adapter<SetAllAdapter.SetsViewHo
     @NonNull
     @NotNull
     @Override
+    //tạo viewholder cho mỗi item trong recyclerview
     public SetAllAdapter.SetsViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(context);
+
+        //liệt kê ItemSetAllBinding với viewholder
         ItemSetAllBinding binding = ItemSetAllBinding.inflate(inflater, parent, false);
         return new SetsViewHolder(binding.getRoot());
     }
 
     @SuppressLint("SetTextI18n")
     @Override
+    //gán dữ liệu từ tập hợp thẻ vào viewholder cho từng vị trí trong danh sách
     public void onBindViewHolder(@NonNull @NotNull SetAllAdapter.SetsViewHolder holder, int position) {
 
-        FlashCard set = sets.get(position);
-        CardDAO cardDAO = new CardDAO(context);
-        int count = cardDAO.countCardByFlashCardId(set.getId());
-        UserDAO userDAO = new UserDAO(context);
-        User user = userDAO.getUserById(set.getUser_id());
+        FlashCard set = sets.get(position);//lấy tập hợp thẻ tại vị trí tương ứng
+        CardDAO cardDAO = new CardDAO(context);//lấy đối tượng CardDAO để đếm số lượng thẻ trong tập hợp
+        int count = cardDAO.countCardByFlashCardId(set.getId());//đếm số lượng thẻ trong tập hợp
+        UserDAO userDAO = new UserDAO(context);//lấy đối tượng UserDAO để lấy thông tin người dùng
+        User user = userDAO.getUserById(set.getUser_id());//lấy thông tin người dùng
 
+        //gán dữ liệu tên bộ thẻ, số lượng, thuật ngữ và tên người tạo vào textview
         holder.binding.setNameTv.setText(set.getName());
         holder.binding.termCountTv.setText(count + " terms");
         holder.binding.userNameTv.setText(user.getUsername());
+
+        //gán ảnh đại diện của người tạo vào imageview
         Picasso.get().load(user.getAvatar()).into(holder.binding.avatarIv);
 
+        //khi người dùng nhấp vào item, chuyển sang ViewActyviti (Xem chi tiết tập hợp thẻ)
         holder.itemView.setOnClickListener(v -> {
-            Intent intent = new Intent(context, ViewSetActivity.class);
-            intent.putExtra("id", set.getId());
+            Intent intent = new Intent(context, ViewSetActivity.class);//tạo intent để chuyển sang ViewSetActivity
+            intent.putExtra("id", set.getId());//gắn id của tập hợp thẻ vào intent
 
-            context.startActivity(intent);
+            context.startActivity(intent);//chuyển sang ViewSetActivity
         });
 
     }
 
     @Override
+    //trả về số lượng các bộ thẻ trong danh sách
     public int getItemCount() {
         return sets.size();
     }
 
+    //quản lý các view của từng item trong recyclerview
     public static class SetsViewHolder extends RecyclerView.ViewHolder {
-        private final ItemSetAllBinding binding;
+        private final ItemSetAllBinding binding;//liên kết layout với viewholder
 
+        //hàm khởi tạo nhận vào view của item và ràng buộc layout
         public SetsViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
-            binding = ItemSetAllBinding.bind(itemView);
+            binding = ItemSetAllBinding.bind(itemView);//ràng buộc layout với item view
         }
     }
 
--- app/src/main/java/com/kewwi/quickmem/adapter/flashcard/SetsAdapter.java
+++ app/src/main/java/com/kewwi/quickmem/adapter/flashcard/SetsAdapter.java
@@ -20,14 +20,17 @@ import com.squareup.picasso.Picasso;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+//hiển thị danh sách các FlashCard (bộ thẻ)
+// với tùy chọn hiển thị theo kiểu thư viện (library) hoặc bình thường.
 
-
+// SetsAdapter: Là adapter quản lý và hiển thị các bộ thẻ trong RecyclerView.
 public class SetsAdapter extends RecyclerView.Adapter<SetsAdapter.SetsViewHolder> {
 
-    private final Context context;
-    private final ArrayList<FlashCard> sets;
-    private final Boolean isLibrary;
+    private final Context context;//truy cập tài nguyên
+    private final ArrayList<FlashCard> sets;//danh sách các bộ thẻ
+    private final Boolean isLibrary;//kiểm tra chế độ hiển thị thư viện
 
+    //hàm khởi tạo adapter với context, danh sách bộ thẻ và chế độ hiển thị
     public SetsAdapter(Context context, ArrayList<FlashCard> sets, Boolean isLibrary) {
         this.context = context;
         this.sets = sets;
@@ -37,54 +40,61 @@ public class SetsAdapter extends RecyclerView.Adapter<SetsAdapter.SetsViewHolder
     @NonNull
     @NotNull
     @Override
+    //tạo viewholder cho mỗi item trong recyclerview
     public SetsAdapter.SetsViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(context);
-        ItemSetBinding binding = ItemSetBinding.inflate(inflater, parent, false);
-        return new SetsViewHolder(binding.getRoot());
+        LayoutInflater inflater = LayoutInflater.from(context);//lấy layout inflater từ context của parent
+        ItemSetBinding binding = ItemSetBinding.inflate(inflater, parent, false);//gắn view cho item trong recyclerview
+        return new SetsViewHolder(binding.getRoot());//trả về viewholder đã được tạo
     }
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n")//để bỏ qua lỗi warning
     @Override
+    //gán dữ liệu từ bộ thẻ vào viewholder cho từng vị trí trong danh sách
     public void onBindViewHolder(@NonNull @NotNull SetsAdapter.SetsViewHolder holder, int position) {
-        if (isLibrary){
-            //set weight of card
-            ViewGroup.LayoutParams params = holder.binding.setCv.getLayoutParams();
-            params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        if (isLibrary) {
+            //hiển thị chế độ thư viện
+            ViewGroup.LayoutParams params = holder.binding.setCv.getLayoutParams();//lấy layout params của item
+            params.width = ViewGroup.LayoutParams.MATCH_PARENT;//đặt chiều rộng bằng MATCH_PARENT
 
         }
-        FlashCard set = sets.get(position);
-        UserSharePreferences userSharePreferences = new UserSharePreferences(context);
-        CardDAO cardDAO = new CardDAO(context);
-        int count = cardDAO.countCardByFlashCardId(set.getId());
-        String avatar = userSharePreferences.getAvatar();
-        String userNames = userSharePreferences.getUserName();
+        FlashCard set = sets.get(position);//lấy bộ thẻ tại vị trí tương ứng
+        UserSharePreferences userSharePreferences = new UserSharePreferences(context);//lấy đối tượng UserSharePreferences để lấy thông tin người dùng
+        CardDAO cardDAO = new CardDAO(context);//lấy đối tượng CardDAO để đếm số lượng thẻ trong tập hợp
+        int count = cardDAO.countCardByFlashCardId(set.getId());//đếm số lượng thẻ trong tập hợp
+        String avatar = userSharePreferences.getAvatar();//lấy ảnh người dùng
+        String userNames = userSharePreferences.getUserName();//lấy thông tin người dùng
 
-        holder.binding.setNameTv.setText(set.getName());
-        holder.binding.termCountTv.setText(count + " terms");
-        holder.binding.userNameTv.setText(userNames);
-        Picasso.get().load(avatar).into(holder.binding.avatarIv);
-        holder.binding.createdDateTv.setText(set.getCreated_at());
+        //gán dữ liệu tên bộ thẻ, số lượng, thuật ngữ và tên người tạo vào textview
+        holder.binding.setNameTv.setText(set.getName());//hiển thị tên bộ thẻ
+        holder.binding.termCountTv.setText(count + " terms");//hiển thị số lượng thuật ngữ
+        holder.binding.userNameTv.setText(userNames);//hiển thị tên người tạo
+        Picasso.get().load(avatar).into(holder.binding.avatarIv);//dùng picasso để tải ảnh người dùng và gán vào imageview
+        holder.binding.createdDateTv.setText(set.getCreated_at());//gán ngày tạo vào textview
 
+        //khi người dùng nhấp vào item, chuyển sang ViewActyviti (Xem chi tiết tập hợp thẻ)
         holder.itemView.setOnClickListener(v -> {
-            Intent intent = new Intent(context, ViewSetActivity.class);
-            intent.putExtra("id", set.getId());
+            Intent intent = new Intent(context, ViewSetActivity.class);//tạo intent để chuyển sang ViewSetActivity
+            intent.putExtra("id", set.getId());//gắn id của tập hợp thẻ vào intent
 
-            context.startActivity(intent);
+            context.startActivity(intent);//chuyển sang ViewSetActivity
         });
 
     }
 
     @Override
+    //trả về số lượng các bộ thẻ trong danh sách
     public int getItemCount() {
         return sets.size();
     }
 
+    //quản lý các view của từng item trong recyclerview
     public static class SetsViewHolder extends RecyclerView.ViewHolder {
-        private final ItemSetBinding binding;
+        private final ItemSetBinding binding;//liên kết layout với viewholder
 
+        //hàm khởi tạo nhận vào view của item và ràng buộc layout
         public SetsViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
-            binding = ItemSetBinding.bind(itemView);
+            binding = ItemSetBinding.bind(itemView);//ràng buộc layout với item view
         }
     }
 
