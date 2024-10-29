4ddfd3d65dd44aef0586e4f0d6750e8ccb857902
Anhiu-0 <natsu7x7x777@gmail.com>
2024-10-29 10:35:03 +0700

    Changes in the adapter

--- app/src/main/java/com/kewwi/quickmem/adapter/card/CardAdapter.java
+++ app/src/main/java/com/kewwi/quickmem/adapter/card/CardAdapter.java
@@ -7,19 +7,26 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.kewwi.quickmem.data.model.Card;
 import com.kewwi.quickmem.databinding.ItemCardAddBinding;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
+//class CardAdapter để quản lý danh sách card trong recyclerview
 public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder> {
-
+    //context đại diện cho ngữ canhr của activity hoặc fragment
     private final Context context;
+
+    //danh sách card
     private final ArrayList<Card> cards;
 
+    //hàm khởi tạo adapter với context và danh sách card
     public CardAdapter(Context context, ArrayList<Card> cards) {
         this.context = context;
         this.cards = cards;
@@ -28,79 +35,104 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder
     @NonNull
     @NotNull
     @Override
+
     public CardAdapter.CardViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        //dùng layouotinflater để chuyển layout từ xml thành view
         LayoutInflater inflater = LayoutInflater.from(context);
+
+        // dùng viewbinding ràng buộc layout item_card_add với code
         ItemCardAddBinding binding = ItemCardAddBinding.inflate(inflater, parent, false);
+
+        //trả về viewholder chứa layout đã ràng buộc
         return new CardViewHolder(binding.getRoot());
     }
 
     @Override
+    //gán dữ liệu từ đối tượng card vào view của viewholder
     public void onBindViewHolder(@NonNull @NotNull CardAdapter.CardViewHolder holder, @SuppressLint("RecyclerView") int position) {
 
+        //lấy đối tượng card tại vị trí tương ứng
         Card card = cards.get(position);
+
+        //xóa các textwatcher cũ để tránh lỗi khi tái sử dụng view
         holder.removeTextWatchers();
 
+        //kiểm tra nếu vị trí > 1 thì yêu cầu focus vào edittext của thẻ trước
         if (position > 1) {
             holder.binding.termEt.requestFocus();
         }
 
+        //đặt dữ liệu từ đối tượng card vào edittext
         holder.binding.termEt.setText(card.getFront());
         holder.binding.definitionEt.setText(card.getBack());
 
 
+        //tạo textwatcher cho phần mặt trước của thẻ
         TextWatcher frontWatcher = new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //cập nhật giá trị mặt trc của thẻ trc khi văn bản thay đổi
                 card.setFront(s.toString().trim());
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //cập nhật mặt trc của thẻ khi văn bản đang thay đổi
                 card.setFront(s.toString().trim());
             }
 
             @Override
             public void afterTextChanged(Editable s) {
+                //cập nhật mặt trc của thẻ sau khi văn bản thay đổi
                 card.setFront(s.toString().trim());
             }
         };
 
+        //tạo textwatcher cho phần mặt sau
         TextWatcher backWatcher = new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //cập nhật mặt sau trc khi thay đổi văn bản
                 card.setBack(s.toString().trim());
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //cập nhật mặt sau khi văn bản đang thay đổi
                 card.setBack(s.toString().trim());
             }
 
             @Override
             public void afterTextChanged(Editable s) {
+                //cập nhật mặt sau sau khi văn bản thay đổi
                 card.setBack(s.toString().trim());
             }
         };
 
+        //gán các textwatcher cho edittext của thẻ
         holder.setTextWatchers(frontWatcher, backWatcher);
 
     }
 
     @Override
+    //trả về tổng số thẻ trong danh sách
     public int getItemCount() {
         return cards.size();
     }
 
+    //lưu trữ view cảu từng item trong recyclerview
     public static class CardViewHolder extends RecyclerView.ViewHolder {
-        private final ItemCardAddBinding binding;
-        private TextWatcher frontWatcher;
-        private TextWatcher backWatcher;
+        private final ItemCardAddBinding binding;//dùng viewbinding để truy cập các view trong layout item_card_add
+        private TextWatcher frontWatcher;//các textwatcher để lưu trữ các thay đổi văn bản trong edittexxt
+        private TextWatcher backWatcher;//các textwatcher để lưu trữ các thay đổi văn bản trong edittexxt
 
+        //hàm khởi tạo viewholder với view được truyền vào, dùng ietmview để ràng buộc các view
         public CardViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
             this.binding = ItemCardAddBinding.bind(itemView);
         }
 
+        //hàm xóa các textwatcher cũ để tránh nhiều textwatcher trùng lặp
         public void removeTextWatchers() {
             if (frontWatcher != null) {
                 binding.termEt.removeTextChangedListener(frontWatcher);
@@ -110,9 +142,12 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder
             }
         }
 
+        //gán mới các textwatcher cho edittexxt
         public void setTextWatchers(TextWatcher frontWatcher, TextWatcher backWatcher) {
             this.frontWatcher = frontWatcher;
             this.backWatcher = backWatcher;
+
+            //gán các textwatcher để lưu trữ thay đổi văn bản
             binding.termEt.addTextChangedListener(frontWatcher);
             binding.definitionEt.addTextChangedListener(backWatcher);
         }
--- app/src/main/java/com/kewwi/quickmem/adapter/card/ViewTermsAdapter.kt
+++ app/src/main/java/com/kewwi/quickmem/adapter/card/ViewTermsAdapter.kt
@@ -9,24 +9,34 @@ import com.kewwi.quickmem.data.model.Card
 import com.kewwi.quickmem.databinding.ItemViewTermsBinding
 import kotlin.collections.ArrayList
 
-class ViewTermsAdapter(
-    private val cards: ArrayList<Card>
-) : RecyclerView.Adapter<ViewTermsAdapter.ViewHolder>() {
-    private var textToSpeech: TextToSpeech? = null
+//quản lý hiển thị và tương tác với temrs trong tập hợp thẻ
 
+// ViewTermsAdapter: Là adapter quản lý và hiển thị các thẻ (Card) trong RecyclerView.
+// Nó cung cấp chức năng chuyển văn bản thành giọng nói (Text-to-Speech) cho các thuật ngữ.
+class ViewTermsAdapter(
+    private val cards: ArrayList<Card>// danh sách các thẻ
+) : RecyclerView.Adapter<ViewTermsAdapter.ViewHolder>() {
+    private var textToSpeech: TextToSpeech? =
+        null//đổi tượng text-to-speech để chuyển văn bản thành giọng nói
+
+    //quả lý các view của item trong recyclerview
     class ViewHolder(val binding: ItemViewTermsBinding) : RecyclerView.ViewHolder(binding.root)
 
+    //tạo viewholder cho mỗi item trong recyclerview
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
+
+        //liên lết layout với viewholder
         val binding = ItemViewTermsBinding.inflate(layoutInflater, parent, false)
         return ViewHolder(binding)
     }
 
+    //gán dữ liệu vào viewholder cho từng vị trí trong danh sách
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val card = cards[position]
+        val card = cards[position]//.lấy thẻ tại vị trí tương ứng
 
-        val status = card.status
-        // change color of card
+        val status = card.status//lấy trạng thái của thẻ
+        // thay đổi màu của thẻ dựa trên trạng thái
         if (status == 1) {
             holder.binding.cardView.setCardBackgroundColor(holder.itemView.context.getColor(android.R.color.holo_green_light))
         } else if (status == 2) {
@@ -35,17 +45,27 @@ class ViewTermsAdapter(
             holder.binding.cardView.setCardBackgroundColor(holder.itemView.context.getColor(android.R.color.white))
         }
 
+        //gán dữ liệu vào textview
         holder.binding.termsTv.text = card.front
         holder.binding.definitionTv.text = card.back
 
+        //khi nhấp vào nút text-to-speech, đọc văn bản của thuật ngữ
         holder.binding.soundIv.setOnClickListener {
             if (holder.binding.termsTv.text.toString().isNotEmpty()) {
+                //cài đặt ngôn ngữ cho text-to-speech
                 textToSpeech = TextToSpeech(holder.itemView.context) { status ->
                     if (status == TextToSpeech.SUCCESS) {
                         val result = textToSpeech?.setLanguage(textToSpeech?.voice?.locale)
+
+                        //kiểm tra ngôn ngữ hỗ trợ và đọc văn bản
                         if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                            Toast.makeText(holder.itemView.context, "Language not supported", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(
+                                holder.itemView.context,
+                                "Language not supported",
+                                Toast.LENGTH_SHORT
+                            ).show()
                         } else {
+                            //cấu hình âm lượng và đọc văn bản
                             textToSpeech?.speak(
                                 holder.binding.termsTv.text.toString(),
                                 TextToSpeech.QUEUE_FLUSH,
@@ -54,7 +74,11 @@ class ViewTermsAdapter(
                             )
                         }
                     } else {
-                        Toast.makeText(holder.itemView.context, "Initialization failed", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            holder.itemView.context,
+                            "Initialization failed",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
             }
@@ -62,11 +86,13 @@ class ViewTermsAdapter(
         }
     }
 
+    //trả về số lượng thẻ trong danh sách
     override fun getItemCount(): Int {
 
         return cards.size
     }
 
+    //khi viewholder bị huỷ, dừng và hủy text-to-speech
     override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
         super.onDetachedFromRecyclerView(recyclerView)
         if (textToSpeech != null) {
