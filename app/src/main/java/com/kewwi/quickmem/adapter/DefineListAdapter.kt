ce5211a16b8f1a22c8725b2726576915ccbd0571
Anhiu-0 <natsu7x7x777@gmail.com>
2024-10-29 17:02:19 +0700

    Changes in the adapter

--- app/src/main/java/com/kewwi/quickmem/adapter/DefineListAdapter.kt
+++ app/src/main/java/com/kewwi/quickmem/adapter/DefineListAdapter.kt
@@ -8,25 +8,35 @@ import com.kewwi.quickmem.R
 import com.kewwi.quickmem.data.model.Card
 import com.kewwi.quickmem.databinding.ItemSelectDefineBinding
 
+//hiển thị danh sách các thẻ (cards) định nghĩa trong RecyclerView
+
+//DefineListAdapter: Adapter quản lý hiển thị danh sách các thẻ định nghĩa.
 class DefineListAdapter(
-    private val cardList: List<Card>,
+    private val cardList: List<Card>,//danh sách các thẻ (cards)
 ) : RecyclerView.Adapter<DefineListAdapter.DefineViewHolder>() {
+    // ViewHolder cho mỗi mục trong danh sách
     class DefineViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // Sử dụng View Binding để truy cập các thành phần giao diện trong item_select_define.xml
         val binding = ItemSelectDefineBinding.bind(itemView)
     }
 
+    //tạo viewholder cho mỗi item trong recyclerview
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DefineViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_select_define, parent, false)
-        return DefineViewHolder(view)
+        // Sử dụng LayoutInflater để inflate layout item_select_define.xml
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_select_define, parent, false)
+        return DefineViewHolder(view)//trả về viewholder mới được tạo
     }
 
+    //trả về số lượng thẻ (cards)
     override fun getItemCount(): Int {
         return cardList.size
     }
 
+    //gán dữ liệu từ thẻ (cards) vào viewholder cho từng vị trí trong danh sách
     override fun onBindViewHolder(holder: DefineViewHolder, position: Int) {
-        val card = cardList[position]
-        holder.binding.wordTv.text = card.front
-        // set other fields as needed
+        val card = cardList[position]//lấy thẻ (cards) tại vị trí tương ứng
+        holder.binding.wordTv.text = card.front//Gán dữ liệu (mặt trước của thẻ) vào TextView wordTv
+
     }
 }
--- app/src/main/java/com/kewwi/quickmem/adapter/viewpager/OnboardingAdapter.java
+++ app/src/main/java/com/kewwi/quickmem/adapter/viewpager/OnboardingAdapter.java
@@ -4,20 +4,27 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
+
 import com.kewwi.quickmem.R;
 import com.kewwi.quickmem.databinding.ItemOnboardingBinding;
-import org.jetbrains.annotations.NotNull;
 
+import org.jetbrains.annotations.NotNull;
+//hiển thị các trang giới thiệu
+
+//OnboardingAdapter: Adapter quản lý hiển thị các trang giới thiệu.
 public class OnboardingAdapter extends PagerAdapter {
 
-    private final Context context;
+    private final Context context;//ngữ cảnh hiện tại
 
+    //hàm khởi tạo adapter
     public OnboardingAdapter(Context context) {
         this.context = context;
     }
 
+    //dữ liệu cho các trang giới thiệu
     private final int[] onBoardingTitles = {
             R.string.onboarding_title_1,
             R.string.onboarding_title_2,
@@ -25,6 +32,7 @@ public class OnboardingAdapter extends PagerAdapter {
             R.string.onboarding_title_4
     };
 
+    //dữ liệu cho các hình ảnh trang giới thiệu
     private final int[] onBoardingImages = {
             R.drawable.onboarding_1,
             R.drawable.ic_start_svg,
@@ -32,32 +40,41 @@ public class OnboardingAdapter extends PagerAdapter {
             R.drawable.onboarding_2
     };
 
+    //trả về số lượng trang giới thiệu
     @Override
     public int getCount() {
         return onBoardingTitles.length;
     }
 
+    //xác định xem một view có đại diện cho trang được trả về bởi instantiateItem hay không
     @Override
     public boolean isViewFromObject(@NonNull @NotNull View view, @NonNull @NotNull Object object) {
-        return view.equals(object);
+        return view.equals(object);//Kiểm tra xem view có khớp với object không
     }
 
+    //tạo view cho mỗi trang giới thiệu
     @NonNull
     @NotNull
     @Override
     public Object instantiateItem(@NonNull @NotNull ViewGroup container, int position) {
+        //// Sử dụng View Binding để inflate layout item_onboarding.xml cho từng trang
         final ItemOnboardingBinding binding = ItemOnboardingBinding.inflate(
+                // Sử dụng LayoutInflater để inflate layout
                 LayoutInflater.from(context), container, false
         );
 
+        //đặt tiêu  đề và hình ảnh cho trang giới thiệu dưa trên vị trí
         binding.onboardingTitleTv.setText(onBoardingTitles[position]);
         binding.onboardingIv.setImageResource(onBoardingImages[position]);
 
+        //thêm view vào container
         container.addView(binding.getRoot());
 
+        //trả về view đã được gắn layout
         return binding.getRoot();
     }
 
+    //hủy view khi không còn hiển thị
     @Override
     public void destroyItem(@NonNull @NotNull ViewGroup container, int position, @NonNull @NotNull Object object) {
         container.removeView((View) object);
