7f901ea08aae796a6d8d190c47954ad095618b56
Anhiu-0 <natsu7x7x777@gmail.com>
2024-11-06 21:57:02 +0700

    Changes in the UI/activities/Profile

--- app/src/main/java/com/kewwi/quickmem/ui/activities/profile/SettingsActivity.java
+++ app/src/main/java/com/kewwi/quickmem/ui/activities/profile/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.kewwi.quickmem.ui.activities.profile;
 
 import android.app.Dialog;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
@@ -29,62 +30,86 @@ import com.saadahmedsoft.popupdialog.listener.OnDialogButtonClickListener;
 
 import java.util.Objects;
 
+// lớp điều khiển cho giao diện cài đặt
 public class SettingsActivity extends AppCompatActivity {
-    private ActivitySettingsBinding binding;
-    private UserSharePreferences userSharePreferences;
-    private AlertDialog detailDialog;
-    UserDAO userDAO;
+    private ActivitySettingsBinding binding; // Khai báo đối tượng binding cho layout của activity
+    private UserSharePreferences userSharePreferences; // Đối tượng dùng để quản lý shared preferences của người dùng
+    private AlertDialog detailDialog; // Hộp thoại để hiển thị các chi tiết thay đổi thông tin người dùng
+    UserDAO userDAO;// Đối tượng Data Access Object dùng để làm việc với dữ liệu người dùng
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Khởi tạo ViewBinding cho activity
         binding = ActivitySettingsBinding.inflate(getLayoutInflater());
+
         final View view = binding.getRoot();
         setContentView(view);
-
+        // Khởi tạo đối tượng quản lý thông tin người dùng từ Shared Preferences
         userSharePreferences = new UserSharePreferences(SettingsActivity.this);
+
+        // Hiển thị tên người dùng hiện tại từ Shared Preferences
         binding.usernameTv.setText(userSharePreferences.getUserName());
+
+        // Hiển thị email người dùng hiện tại từ Shared Preferences
         binding.emailTv.setText(userSharePreferences.getEmail());
 
-        onClickItemSetting();
+        onClickItemSetting();// Gọi hàm thiết lập sự kiện khi người dùng tương tác với các tùy chọn
 
+        // Thiết lập toolbar
         setSupportActionBar(binding.toolbar);
+
+        // Đặt sự kiện quay lại khi nhấn vào nút quay lại trên toolbar
         binding.toolbar.setNavigationOnClickListener(v -> finish());
     }
 
+    // Hàm thiết lập sự kiện khi người dùng nhấn vào các tùy chọn trong phần cài đặt
     private void onClickItemSetting() {
+        // Khi nhấn vào tùy chọn thay đổi tên, mở hộp thoại thay đổi tên
         binding.usernameCl.setOnClickListener(view -> openDialogChangeUsername());
+
+        // Khi nhấn vào tùy chọn thay đổi email, mở hộp thoại thay đổi email
         binding.emailCl.setOnClickListener(view -> openDialogChangeEmail());
+
+        // Khi nhấn vào tùy chọn thay đổi mật khẩu, mở activity thay đổi mật khẩu
         binding.passwordCl.setOnClickListener(view -> {
             startActivity(new Intent(SettingsActivity.this, ChangePasswordActivity.class));
         });
 
+        // Khi nhấn vào nút đăng xuất
         binding.logOutBtn.setOnClickListener(v -> {
             PopupDialog.getInstance(SettingsActivity.this)
-                    .setStyle(Styles.STANDARD)
-                    .setHeading("Log out!")
-                    .setDescription("Are you sure")
-                    .setPopupDialogIcon(R.drawable.baseline_logout_24)
-                    .setCancelable(true)
-                    .setPositiveButtonText("OK")
+                    .setStyle(Styles.STANDARD)// Thiết lập kiểu popup
+                    .setHeading("Log out!") // Tiêu đề của popup
+                    .setDescription("Are you sure")// Mô tả yêu cầu xác nhận
+                    .setPopupDialogIcon(R.drawable.baseline_logout_24) // Đặt biểu tượng cho popup
+                    .setCancelable(true) // Cho phép popup có thể bị hủy bỏ
+                    .setPositiveButtonText("OK") // Đặt nhãn cho nút xác nhận
                     .showDialog(new OnDialogButtonClickListener() {
                         @Override
                         public void onPositiveClicked(Dialog dialog) {
                             super.onPositiveClicked(dialog);
+                            // Lấy thông tin người dùng
                             userSharePreferences = new UserSharePreferences(SettingsActivity.this);
+
+                            // Xóa thông tin người dùng từ Shared Preferences khi đăng xuất
                             userSharePreferences.clear();
+
+                            // Chuyển về màn hình đăng nhập
                             Intent intent = new Intent(SettingsActivity.this, SignInActivity.class);
                             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                            startActivity(intent);
-                            finish();
+                            startActivity(intent); // Khởi chạy Intent để mở màn hình đăng nhập
+                            finish(); // Kết thúc Activity hiện tại
                         }
 
                         @Override
                         public void onNegativeClicked(Dialog dialog) {
                             super.onNegativeClicked(dialog);
-                            dialog.dismiss();
+                            dialog.dismiss();// Đóng popup nếu người dùng chọn hủy
                         }
                     });
 
@@ -92,65 +117,83 @@ public class SettingsActivity extends AppCompatActivity {
         });
     }
 
+    // Mở hộp thoại thay đổi email
     private void openDialogChangeEmail() {
+        // Khởi tạo đối tượng DAO để truy xuất dữ liệu người dùng
         userDAO = new UserDAO(SettingsActivity.this);
+        // Khởi tạo ViewBinding cho hộp thoại
         DialogChangeEmailBinding changeEmailBinding = DialogChangeEmailBinding.inflate(LayoutInflater.from(SettingsActivity.this));
         View view = changeEmailBinding.getRoot();
 
+        // Tạo đối tượng AlertDialog
         AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
         builder.setView(view);
-        detailDialog = builder.create();
-        detailDialog.setCanceledOnTouchOutside(false);
-        detailDialog.show();
-        detailDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        detailDialog = builder.create(); // Khởi tạo hộp thoại
+        detailDialog.setCanceledOnTouchOutside(false); // Đặt hộp thoại không tự đóng khi nhấn bên ngoài
+        detailDialog.show();// Hiển thị hộp thoại
+        detailDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));// Đặt nền hộp thoại trong suốt
 
-        changeEmailBinding.cancelChangeEmailBtn.setOnClickListener(view1 -> detailDialog.dismiss());
+        changeEmailBinding.cancelChangeEmailBtn.setOnClickListener(view1 -> detailDialog.dismiss());// Đóng hộp thoại khi nhấn hủy
 
+        // Xử lý khi người dùng nhấn nút xác nhận thay đổi email
         changeEmailBinding.submitChangeEmailBtn.setOnClickListener(v -> {
 //            startActivity(new Intent(SettingsActivity.this, ChangeEmailActivity.class));
+
+            // Lấy thông tin người dùng từ Shared Preferences
             String password = changeEmailBinding.passwordEt.getText().toString().trim();
+
+
             userSharePreferences = new UserSharePreferences(SettingsActivity.this);
             String id = userSharePreferences.getId();
             if (password.isEmpty()) {
+                // Kiểm tra mật khẩu trống
                 changeEmailBinding.textIL.setHelperText("Please enter your password");
             } else if (!Objects.equals(PasswordHasher.hashPassword(password), userDAO.getPasswordUser(id))) {
                 Toast.makeText(this, "Pass: " + userDAO.getPasswordUser(id), Toast.LENGTH_SHORT).show();
-                changeEmailBinding.textIL.setHelperText("Password is incorrect");
+                changeEmailBinding.textIL.setHelperText("Password is incorrect");// Thông báo sai mật khẩu
             } else {
                 changeEmailBinding.textIL.setHelperText("");
+                // Mở activity thay đổi email nếu mật khẩu đúng
                 startActivity(new Intent(SettingsActivity.this, ChangeEmailActivity.class));
                 detailDialog.dismiss();
             }
         });
     }
 
+    // Mở hộp thoại thay đổi tên người dùng
     private void openDialogChangeUsername() {
+        // Khởi tạo đối tượng DAO để truy xuất dữ liệu người dùng
         userDAO = new UserDAO(SettingsActivity.this);
+        // Khởi tạo ViewBinding cho hộp thoại
         DialogChangeUsernameBinding changeUsernameBinding = DialogChangeUsernameBinding.inflate(LayoutInflater.from(SettingsActivity.this));
+
         View view = changeUsernameBinding.getRoot();
 
+        // Tạo đối tượng AlertDialog
         AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
         builder.setView(view);
-        detailDialog = builder.create();
-        detailDialog.setCanceledOnTouchOutside(false);
-        detailDialog.show();
+        detailDialog = builder.create();// Khởi tạo hộp thoại
+        detailDialog.setCanceledOnTouchOutside(false);// Đặt hộp thoại không tự đóng khi nhấn bên ngoài
+        detailDialog.show();// Hiển thị hộp thoại
         detailDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
-        changeUsernameBinding.cancelChangeName.setOnClickListener(view1 -> detailDialog.dismiss());
+        changeUsernameBinding.cancelChangeName.setOnClickListener(view1 -> detailDialog.dismiss());// Đóng hộp thoại khi nhấn hủy
 
+        // Xử lý khi người dùng nhấn nút xác nhận thay đổi tên
         changeUsernameBinding.submitChangeName.setOnClickListener(v -> {
-//            startActivity(new Intent(SettingsActivity.this, ChangeUsernameActivity.class));
-            String password = changeUsernameBinding.passwordEt.getText().toString().trim();
-            userSharePreferences = new UserSharePreferences(SettingsActivity.this);
+//           startActivity(new Intent(SettingsActivity.this, ChangeUsernameActivity.class));
+            String password = changeUsernameBinding.passwordEt.getText().toString().trim();// Lấy mật khẩu từ ô nhập liệu
+            userSharePreferences = new UserSharePreferences(SettingsActivity.this); // Lấy thông tin người dùng từ Shared Preferences
             String id = userSharePreferences.getId();
             if (password.isEmpty()) {
-                changeUsernameBinding.textIL.setHelperText("Please enter your password");
+                changeUsernameBinding.textIL.setHelperText("Please enter your password"); // Kiểm tra mật khẩu trống
             } else if (!Objects.equals(PasswordHasher.hashPassword(password), userDAO.getPasswordUser(id))) {
-                changeUsernameBinding.textIL.setHelperText("Password is incorrect");
+                changeUsernameBinding.textIL.setHelperText("Password is incorrect");// Thông báo sai mật khẩu
             } else {
                 changeUsernameBinding.textIL.setHelperText("");
+                // Mở activity thay đổi tên nếu mật khẩu đúng
                 startActivity(new Intent(SettingsActivity.this, ChangeUsernameActivity.class));
-                detailDialog.dismiss();
+                detailDialog.dismiss();// Đóng hộp thoại
             }
         });
     }
--- app/src/main/java/com/kewwi/quickmem/ui/activities/profile/change/ChangePasswordActivity.java
+++ app/src/main/java/com/kewwi/quickmem/ui/activities/profile/change/ChangePasswordActivity.java
@@ -17,8 +17,10 @@ import com.kewwi.quickmem.preferen.UserSharePreferences;
 import com.kewwi.quickmem.utils.PasswordHasher;
 
 import java.util.Objects;
-
+//lớp điều khiển cho giao diện thay đổi mật khẩu của người dùng
+// Lớp ChangePasswordActivity kế thừa từ AppCompatActivity để tạo một màn hình thay đổi mật khẩu người dùng
 public class ChangePasswordActivity extends AppCompatActivity {
+    // Khai báo các biến toàn cục
     private ActivityChangePasswordBinding binding;
     private UserDAO userDAO;
     String currentPass, newPass, reNewPass, id;
@@ -26,68 +28,95 @@ public class ChangePasswordActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Sử dụng View Binding để gán layout cho Activity
         binding = ActivityChangePasswordBinding.inflate(getLayoutInflater());
         final View view = binding.getRoot();
         setContentView(view);
 
+        // Thiết lập Toolbar và thêm chức năng quay lại khi người dùng bấm nút điều hướng
         setSupportActionBar(binding.toolbar);
         binding.toolbar.setNavigationOnClickListener(v -> getOnBackPressedDispatcher().onBackPressed());
 
+        // Lấy ID người dùng từ UserSharePreferences
         UserSharePreferences userSharePreferences = new UserSharePreferences(this);
         id = userSharePreferences.getId();
 
+        // Lắng nghe sự thay đổi khi người dùng rời khỏi các ô nhập liệu
         binding.currentPassEt.setOnFocusChangeListener((view1, b) -> {
             if (!b) {
-                validCurrentPass();
+                validCurrentPass();// Kiểm tra mật khẩu hiện tại
             }
         });
         binding.newPassEt.setOnFocusChangeListener((view12, b) -> {
             if (!b) {
-                validNewPass();
+                validNewPass();// Kiểm tra mật khẩu mới
             }
         });
         binding.confirmPassEt.setOnFocusChangeListener((view13, b) -> {
             if (!b) {
-                validConfirmPass();
+                validConfirmPass(); // Kiểm tra xác nhận mật khẩu
             }
         });
     }
 
+    // Phương thức thay đổi mật khẩu
     private void changePassword() {
-        validConfirmPass();
-        validNewPass();
-        validConfirmPass();
+        validConfirmPass();// Kiểm tra xác nhận mật khẩu
+        validNewPass();// Kiểm tra mật khẩu mới
+        validConfirmPass();// Kiểm tra xác nhận mật khẩu
 
+        // Nếu không có lỗi trợ giúp nào trong các trường mật khẩu
         if (binding.currentPassIL.getHelperText() == null &&
                 binding.newPassIL.getHelperText() == null &&
                 binding.currentPassIL.getHelperText() == null) {
 
+            // Mã hóa mật khẩu mới và cập nhật nó trong cơ sở dữ liệu
             userDAO = new UserDAO(this);
             String hashedPassword = PasswordHasher.hashPassword(newPass);
             if (userDAO.updatePasswordUser(id, hashedPassword) > 0) {
                 Toast.makeText(this, "Change username SUCCESS", Toast.LENGTH_SHORT).show();
-                getOnBackPressedDispatcher().onBackPressed();
+                getOnBackPressedDispatcher().onBackPressed();// Quay lại màn hình trước đó
             } else {
                 Toast.makeText(this, "Change username UNSUCCESSFUL", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
+    // Phương thức kiểm tra xác nhận mật khẩu
     private void validConfirmPass() {
+        // Lấy dữ liệu từ trường xác nhận mật khẩu, loại bỏ khoảng trắng thừa
         reNewPass = binding.confirmPassEt.getText().toString().trim();
+
+        // Kiểm tra nếu trường nhập xác nhận mật khẩu trống
         if (reNewPass.isEmpty()) {
+            // Nếu trống, hiển thị thông báo yêu cầu nhập mật khẩu mới
             binding.confirmPassIL.setHelperText("Please enter your new password");
+
+            // Đặt màu cho thông báo lỗi thành màu đỏ
             binding.confirmPassIL.setHelperTextColor(ColorStateList.valueOf(Color.RED));
+
+            // Đặt con trỏ vào trường nhập xác nhận mật khẩu để người dùng nhập lại
             binding.confirmPassEt.requestFocus();
-        } else if (!reNewPass.equals(newPass)) {
+        }
+        // Kiểm tra nếu xác nhận mật khẩu không khớp với mật khẩu mới
+        else if (!reNewPass.equals(newPass)) {
+            // Nếu không khớp, hiển thị thông báo lỗi về việc xác nhận mật khẩu phải giống với mật khẩu mới
             binding.confirmPassIL.setHelperText("Confirm password must be similar new password");
+
+            // Đặt màu cho thông báo lỗi thành màu đỏ
             binding.confirmPassIL.setHelperTextColor(ColorStateList.valueOf(Color.RED));
+
+            // Đặt con trỏ vào trường nhập xác nhận mật khẩu để người dùng nhập lại
             binding.confirmPassEt.requestFocus();
-        } else {
+        }
+        // Nếu tất cả hợp lệ
+        else {
+            // Xóa thông báo lỗi
             binding.confirmPassIL.setHelperText(null);
         }
     }
 
+    // Phương thức kiểm tra mật khẩu mới
     private void validNewPass() {
         newPass = binding.newPassEt.getText().toString().trim();
         if (newPass.isEmpty()) {
@@ -107,33 +136,57 @@ public class ChangePasswordActivity extends AppCompatActivity {
         }
     }
 
+    // Phương thức kiểm tra mật khẩu hiện tại
     private void validCurrentPass() {
+        // Khởi tạo đối tượng UserDAO để truy xuất dữ liệu người dùng từ cơ sở dữ liệu
         userDAO = new UserDAO(this);
+
+        // Lấy mật khẩu hiện tại mà người dùng đã nhập và loại bỏ khoảng trắng thừa
         currentPass = binding.currentPassEt.getText().toString().trim();
 
+        // Kiểm tra nếu trường nhập mật khẩu hiện tại đang trống
         if (currentPass.isEmpty()) {
+            // Nếu trống, hiển thị thông báo yêu cầu nhập mật khẩu
             binding.currentPassIL.setHelperText("Please enter your password");
+
+            // Đặt màu cho thông báo lỗi thành màu đỏ
             binding.currentPassIL.setHelperTextColor(ColorStateList.valueOf(Color.RED));
+
+            // Đặt con trỏ vào trường nhập mật khẩu hiện tại để người dùng nhập lại
             binding.currentPassEt.requestFocus();
-        } else if (!Objects.equals(PasswordHasher.hashPassword(currentPass), userDAO.getPasswordUser(id))) {
+        }
+        // Kiểm tra nếu mật khẩu hiện tại không khớp với mật khẩu trong cơ sở dữ liệu
+        else if (!Objects.equals(PasswordHasher.hashPassword(currentPass), userDAO.getPasswordUser(id))) {
+            // Hiển thị một thông báo ngắn cho biết mật khẩu không chính xác
             Toast.makeText(this, "Pass: " + userDAO.getPasswordUser(id), Toast.LENGTH_SHORT).show();
+
+            // Hiển thị thông báo lỗi về việc mật khẩu không đúng
             binding.currentPassIL.setHelperText("Password is incorrect");
+
+            // Đặt màu cho thông báo lỗi thành màu đỏ
             binding.currentPassIL.setHelperTextColor(ColorStateList.valueOf(Color.RED));
+
+            // Đặt con trỏ vào trường nhập mật khẩu hiện tại để người dùng nhập lại
             binding.currentPassEt.requestFocus();
-        } else {
+        }
+        // Nếu mật khẩu hiện tại hợp lệ
+        else {
+            // Xóa thông báo lỗi (nếu có)
             binding.currentPassIL.setHelperText(null);
         }
     }
 
+    // Tạo menu có nút "Lưu"
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_save, menu);
         return true;
     }
 
+    // Xử lý khi người dùng chọn mục menu
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.save) {
-            // code save
+            // Nếu người dùng chọn "Lưu", gọi phương thức thay đổi mật khẩu
             changePassword();
             return true;
         } else {
